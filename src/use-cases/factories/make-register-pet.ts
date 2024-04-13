a3f43c958b36b8bf8e6987c375819b963dae528e
EverSilva <everson.silva2706@gmail.com>
2024-04-13 12:12:54 -0300

    feat: add factories to register pet use case

---
+++ src/use-cases/factories/make-register-pet.ts
@@ -0,0 +1,12 @@
+import { PrismaOrganizationsRepository } from "../../repositories/prisma/prisma-organizations-repository"
+import { PrismaPetsRepository } from "../../repositories/prisma/prisma-pets-repository"
+import { RegisterPetUseCase } from "../register-pet"
+
+export const makeRegisterPetUseCase = () => {
+    const petsRepository = new PrismaPetsRepository()
+    const organizationsRepository = new PrismaOrganizationsRepository()
+
+    const registerPetUseCase = new RegisterPetUseCase(petsRepository, organizationsRepository)
+    
+    return registerPetUseCase
+}
